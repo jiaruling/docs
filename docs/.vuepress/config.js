c96a779c34fdfc2556dffad0803f6fb9d896b131
jiaruling <56283208+jiaruling@users.noreply.github.com>
2022-03-14 23:29:17 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   lang: 'zh-CN',
   title: 'VuePress',
   description: '这是我的第一个 VuePress 站点',
-  base: '/',
+  base: './',
   actionsText: "快速开始",
   actionsLink: "/",
   port: 8089, // 端口
